bb2768decf64e67f2099ce4ec34bbaae113502a0
Deepak Kumar Sharma <deepak.khandal3@gmail.com>
2021-01-03 23:48:47 +0530

    index.ejs file loaded

--- functions/index.js
+++ functions/index.js
@@ -3,7 +3,10 @@ const functions = require('firebase-functions');
 const express = require('express');
 const cors = require('cors');
 const app = express();
+app.set('view engine','ejs');
 app.use(cors({ origin: true }));
+app.use(express.urlencoded({extended:true}));
+app.use(express.static('public'));
 
 // Test API
 app.get('/api-test', (req, res) => {
@@ -21,8 +24,9 @@ exports.app = functions.https.onRequest(app);
 const cont  = new controller();
 
 // Load admin panel
-
-
+app.get('/',(request, response) =>{
+	response.render('index', { title: 'Employee Adminss Login' });
+});
 
 
 app.get('/api/getemployees', (req, res) => { cont.findAllEmployees(req, res)});
